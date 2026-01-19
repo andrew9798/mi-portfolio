a8ef4abaf9e004a2ca59bd39d06afd77fe8f57bc
andrew9798 <andresganga1298@gmail.com>
2026-01-19 11:07:39 +0100

    create translate files i118 de/es/en

---
+++ src/i118n/en.ts
@@ -0,0 +1,42 @@
+export const en = {
+  navInicio: "Home",
+  navAcerca: "About Me",
+  navProyectos: "Projects",
+  navHabilidades: "Skills",
+  navContacto: "Contact",
+
+  heroTitulo: "AndresDev",
+  heroDescripcion: "Full-Stack Developer specialized in creating exceptional web experiences",
+  heroVerProyectos: "See Projects",
+
+  acercaTitulo: "About Me",
+  acercaDescripcion: "My name is Andrés Escámez Ganga and I am a passionate web developer focused on building elegant and functional digital solutions. With over 5 years of experience, I specialize in full-stack development using modern technologies such as React, Node.js, Astro and Tailwind CSS among other tools. My approach blends technical precision with thoughtful design, inspired by traditional craftsmanship to create timeless web experiences.",
+  acercaGithub: "GitHub",
+  acercaLinkedIn: "LinkedIn",
+
+  proyectosDestacados: "Featured Projects",
+  proyectoFinanzas: "Personal Finance Manager",
+  proyectoFinanzasDescripcion: "Interactive dashboard for data analysis with dynamic visualizations and real-time reports.",
+  proyectoDemo: "Demo",
+  proyectoCodigo: "Code",
+
+  habilidadesTitulo: "Skills & Knowledge",
+  habilidadesHTML: "HTML",
+  habilidadesCSS: "CSS",
+  habilidadesJS: "JavaScript",
+  habilidadesReact: "React",
+  habilidadesNode: "Node.js",
+  habilidadesAstro: "Astro",
+  habilidadesTailwind: "Tailwind CSS",
+  habilidadesMongo: "MongoDB",
+  habilidadesMySQL: "MySQL",
+  habilidadesGit: "Git",
+  habilidadesExpress: "Express",
+
+  contactoTitulo: "Contact",
+  contactoPregunta: "Do you have a project in mind? I’d love to collaborate with you to create something extraordinary.",
+  contactoEnviar: "Send Message",
+  contactoAviso: "By submitting, you agree that I may use your email to reply. I do not share your information with third parties.",
+
+  pie: "© 2025 Andrés Escámez Ganga. All rights reserved."
+};
